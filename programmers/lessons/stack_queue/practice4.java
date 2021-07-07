648bade59ac905de18cf835b815ae16a6c33980d
sangwoongLee <litty17@naver.com>
2021-07-08 00:55:02 +0900

    Create practice4.java

---
+++ programmers/lessons/stack_queue/practice4.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+# 프로그래머스 문제 - 주식거래
+
+class Solution {
+    class Data{
+        Integer time;
+        Integer price;
+        Data(Integer time, Integer price){
+            this.time = time;
+            this.price = price;
+        }
+    }
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Data> stack = new Stack();
+
+        int now = 0;
+
+
+        for(int price: prices){
+
+            while(!stack.isEmpty() && price < stack.peek().price){
+                int current = stack.pop().time;
+                answer[current] = now - current;
+            }
+
+            stack.push(new Data(now, price));
+            now++;
+        }
+
+        while(!stack.isEmpty()) {
+            int current = stack.pop().time;
+            answer[current] = now-current-1;
+        }
+
+        return answer;
+    }
+}
